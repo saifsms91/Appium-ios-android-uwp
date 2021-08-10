618f3d9befc832f7e508c1ad153572ee9b0d55ff
ssiddiqui <saif.siddiqui@luminixinc.com>
2021-08-10 18:36:11 +0800

    - Added retry logic

---
+++ src/main/java/extentListeners/Retry.java
@@ -0,0 +1,31 @@
+package extentListeners;
+
+import base.*;
+import lib.AppLib;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import static base.ScreenBase.driver;
+
+public class Retry implements IRetryAnalyzer {
+
+    private int count = 0;
+    private static int maxTry = 1; //Run the failed test 2 times
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (!iTestResult.isSuccess()) {                     //Check if test not succeed
+            driver.closeApp();
+            driver.launchApp();
+            if (count < maxTry) {                           //Check if maxTry count is reached
+                count++;                                    //Increase the maxTry count by 1
+                iTestResult.setStatus(ITestResult.FAILURE); //Mark test as failed and take base64Screenshot
+                // extendReportsFailOperations(iTestResult);   //ExtentReports fail operations
+                return true;                                //Tells TestNG to re-run the test
+            }
+        } else {
+            iTestResult.setStatus(ITestResult.SUCCESS);     //If test passes, TestNG marks it as passed
+        }
+        return false;
+    }
+}
